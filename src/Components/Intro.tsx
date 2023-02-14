ef23cb9369e59c2fd7b00edccef18d5f54c1c4aa
Ayaz Uddin <ayazuddin@ayazs-air.lan>
2023-02-14 08:52:02 -0500

    update

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -6,7 +6,12 @@ import { NameAnimation, Title } from "@/Components";
 const Intro = () => {
   return (
     <Box
-      sx={{ height: "100vh", backgroundColor: "salmon", minHeight: "800px" }}
+      sx={{
+        height: "100vh",
+        backgroundColor: "salmon",
+        minHeight: "800px",
+        position: "relative",
+      }}
     >
       <Title />
       <NameAnimation />
--- src/Components/NameAnimation.tsx
+++ src/Components/NameAnimation.tsx
@@ -1,54 +1,131 @@
 import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
+import { useRef } from "react";
 
 const outterstyle = {
   position: "absolute",
-  left: "50%",
-  top: "50%",
-  transform: "translate(-50%, -50%)",
+  left: "10%",
+  top: "40%",
   zIndex: "2",
 };
 
 const style = {
+  position: "relative",
   zIndex: "5",
-  fontSize: "80px",
+  fontSize: "60px",
   color: "#fff",
+  opacity: 0,
+};
+
+const secondaryStyle = {
+  zIndex: "5",
+  fontSize: "20px",
+  color: "#fff",
+  opacity: 0,
 };
 
 const NameAnimation = () => {
+  const letter2: any = useRef(null);
+  const letter3: any = useRef(null);
+  const letter4: any = useRef(null);
+  const letter5: any = useRef(null);
+  const letter6: any = useRef(null);
+  const letter7: any = useRef(null);
+  const letter8: any = useRef(null);
+  const letter9: any = useRef(null);
+  const letter10: any = useRef(null);
+  const letter11: any = useRef(null);
+  const title: any = useRef(null);
+
+  const handleAnimationEnd = (ref: any, className: string) => {
+    if (ref.current) {
+      console.log(ref);
+      ref.current.className = `${ref.current.className} ${className}`;
+    }
+  };
+
   return (
     <Box sx={outterstyle}>
       <Box sx={{ display: "flex" }}>
-        <Typography className="letter-1" sx={style}>
+        <Typography
+          className="letter1"
+          sx={style}
+          onAnimationEnd={() => handleAnimationEnd(letter2, "letter2")}
+        >
           A
         </Typography>
-        <Typography className="letter-2" sx={style}>
+        <Typography
+          sx={style}
+          ref={letter2}
+          onAnimationEnd={() => handleAnimationEnd(letter3, "letter3")}
+        >
           Y
         </Typography>
-        <Typography className="letter-3" sx={style}>
+        <Typography
+          sx={style}
+          ref={letter3}
+          onAnimationEnd={() => handleAnimationEnd(letter4, "letter4")}
+        >
           A
         </Typography>
-        <Typography className="letter-4" sx={style}>
+        <Typography
+          sx={style}
+          ref={letter4}
+          onAnimationEnd={() => handleAnimationEnd(letter5, "letter5")}
+        >
           Z
         </Typography>
         <Typography
-          className="letter-5"
           sx={{ fontSize: "80px", color: "#fff", width: "30px" }}
+          ref={letter5}
+          onAnimationEnd={() => handleAnimationEnd(letter6, "letter6")}
         ></Typography>
-        <Typography className="letter-6" sx={style}>
+        <Typography
+          sx={style}
+          ref={letter6}
+          onAnimationEnd={() => handleAnimationEnd(letter7, "letter7")}
+        >
           U
         </Typography>
-        <Typography className="letter-7" sx={style}>
+        <Typography
+          sx={style}
+          ref={letter7}
+          onAnimationEnd={() => handleAnimationEnd(letter8, "letter8")}
+        >
           D
         </Typography>
-        <Typography className="letter-8" sx={style}>
+        <Typography
+          sx={style}
+          ref={letter8}
+          onAnimationEnd={() => handleAnimationEnd(letter9, "letter9")}
+        >
           D
         </Typography>
-        <Typography className="letter-9" sx={style}>
+        <Typography
+          sx={style}
+          ref={letter9}
+          onAnimationEnd={() => handleAnimationEnd(letter10, "letter10")}
+        >
           I
         </Typography>
-        <Typography className="letter-10" sx={style}>
+        <Typography
+          sx={style}
+          ref={letter10}
+          onAnimationEnd={() => handleAnimationEnd(letter11, "letter11")}
+        >
           N
+        </Typography>
+        <Typography
+          sx={style}
+          ref={letter11}
+          onAnimationStart={() => handleAnimationEnd(title, "title")}
+        >
+          _
+        </Typography>
+      </Box>
+      <Box>
+        <Typography sx={secondaryStyle} ref={title}>
+          Frontend Software Engineer
         </Typography>
       </Box>
     </Box>
