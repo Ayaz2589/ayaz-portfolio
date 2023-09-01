38e28251f3a01b615c860159ed66ee94811e0dd0
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-09-01 16:51:55 -0400

    update

--- src/Components/Resume.tsx
+++ src/Components/Resume.tsx
@@ -1,8 +1,8 @@
 import { Box, Text, Container, Button } from "@chakra-ui/react";
+import { isMobile } from "react-device-detect";
 
 const style = {
   height: "100vh",
-  backgroundColor: "#eee",
   minHeight: "800px",
 };
 
@@ -21,7 +21,12 @@ const Resume = ({ resumeRef, scrollPosition }: IProps) => {
   return (
     <Box ref={resumeRef} sx={style}>
       <Container
-        sx={{ width: "100%", position: "relative", opacity: 0 }}
+        sx={{
+          width: "100%",
+          position: "relative",
+          opacity: 0,
+          margin: "1rem 0",
+        }}
         className={
           scrollPosition > 1500
             ? `paragraph-animation-appear`
@@ -29,79 +34,85 @@ const Resume = ({ resumeRef, scrollPosition }: IProps) => {
         }
       >
         <Text
-          sx={{ fontSize: "3rem", fontWeight: "light", padding: "2rem 0rem" }}
+          sx={{
+            fontSize: "3rem",
+            fontWeight: "light",
+            padding: {
+              base: "1.5rem 0rem 0rem 0rem",
+              md: "5rem 0rem 0rem 0rem",
+            },
+            margin: { base: "0", md: "1.5rem 0" },
+          }}
         >
           Resume
         </Text>
       </Container>
-      <Box>
-        <Container
-          sx={{ marging: "0rem 1rem", position: "relative", opacity: 0 }}
-          className={
-            scrollPosition > 1500
-              ? `paragraph-animation-appear-delay-1`
-              : `paragraph-animation-disappear`
-          }
-        >
-          <Text sx={{ fontSize: "1.4rem", fontWeight: "light" }}>Design</Text>
-          <Text sx={{ fontSize: "0.9rem" }}>
-            Illustration & UI Graphics, Strategy & Vision Presentation, User
-            Flows, Task Flows, Concept Sketches, Wireframes, Figma Advance
-            Features (Auto-Layout, Components, Variables, Smart Animation),
-            Rapid Prototyping using Figma.
-          </Text>
-        </Container>
-        <Container
-          sx={mobileContainerStyle}
-          className={
-            scrollPosition > 1500
-              ? `paragraph-animation-appear-delay-2`
-              : `paragraph-animation-disappear`
-          }
-        >
-          <Text sx={{ fontSize: "1.4rem", fontWeight: "light" }}>
-            Development
-          </Text>
-          <Text sx={{ fontSize: "0.9rem" }}>
-            HTML, CSS, Javascript (Next.js, React, Node.js), Git, VSCode,
-            Storybook.
-          </Text>
-        </Container>
-        <Container
-          sx={mobileContainerStyle}
-          className={
-            scrollPosition > 1500
-              ? `paragraph-animation-appear-delay-3`
-              : `paragraph-animation-disappear`
-          }
-        >
-          <Text sx={{ fontSize: "1.4rem", fontWeight: "light" }}>Research</Text>
-          <Text sx={{ fontSize: "0.9rem" }}>
-            Data Analysis (Data Dog, Statista), User Interviews, A/B Testing,
-            User Research Reports, User Research Presentations.
-          </Text>
-        </Container>
-        <Container
-          sx={mobileContainerStyle}
-          className={
-            scrollPosition > 1500
-              ? `paragraph-animation-appear-delay-4`
-              : `paragraph-animation-disappear`
-          }
-        >
-          <Text sx={{ fontSize: "1.4rem", fontWeight: "light" }}>
-            Collaboration
-          </Text>
-          <Text sx={{ fontSize: "0.9rem" }}>
-            Facilitaing Design Critiques, Self Starter, Agile, Scrum, JIRA
-          </Text>
-        </Container>
-      </Box>
+      <Container
+        sx={{ position: "relative", opacity: 0, margin: "1.5rem 0" }}
+        className={
+          scrollPosition > 1500
+            ? `paragraph-animation-appear-delay-1`
+            : `paragraph-animation-disappear`
+        }
+      >
+        <Text sx={{ fontSize: "1.4rem", fontWeight: "light" }}>Design</Text>
+        <Text sx={{ fontSize: "0.9rem" }}>
+          Illustration & UI Graphics, Strategy & Vision Presentation, User
+          Flows, Task Flows, Concept Sketches, Wireframes, Figma Advance
+          Features (Auto-Layout, Components, Variables, Smart Animation), Rapid
+          Prototyping using Figma.
+        </Text>
+      </Container>
+      <Container
+        sx={mobileContainerStyle}
+        className={
+          scrollPosition > 1500
+            ? `paragraph-animation-appear-delay-2`
+            : `paragraph-animation-disappear`
+        }
+      >
+        <Text sx={{ fontSize: "1.4rem", fontWeight: "light" }}>
+          Development
+        </Text>
+        <Text sx={{ fontSize: "0.9rem" }}>
+          HTML, CSS, Javascript (Next.js, React, Node.js), Git, VSCode,
+          Storybook.
+        </Text>
+      </Container>
+      <Container
+        sx={mobileContainerStyle}
+        className={
+          scrollPosition > 1500
+            ? `paragraph-animation-appear-delay-3`
+            : `paragraph-animation-disappear`
+        }
+      >
+        <Text sx={{ fontSize: "1.4rem", fontWeight: "light" }}>Research</Text>
+        <Text sx={{ fontSize: "0.9rem" }}>
+          Data Analysis (Data Dog, Statista), User Interviews, A/B Testing, User
+          Research Reports, User Research Presentations.
+        </Text>
+      </Container>
+      <Container
+        sx={mobileContainerStyle}
+        className={
+          scrollPosition > 1500
+            ? `paragraph-animation-appear-delay-4`
+            : `paragraph-animation-disappear`
+        }
+      >
+        <Text sx={{ fontSize: "1.4rem", fontWeight: "light" }}>
+          Collaboration
+        </Text>
+        <Text sx={{ fontSize: "0.9rem" }}>
+          Facilitaing Design Critiques, Self Starter, Agile, Scrum, JIRA
+        </Text>
+      </Container>
       <Button
         variant="ghost"
         sx={{
           color: "#E0921F",
-          margin: "1rem 0rem",
+          margin: "rem 0rem",
           opacity: 0,
           position: "relative",
         }}
@@ -115,6 +126,10 @@ const Resume = ({ resumeRef, scrollPosition }: IProps) => {
       </Button>
     </Box>
   );
+};
+
+const DesktopIcons = () => {
+  return;
 };
 
 export default Resume;
