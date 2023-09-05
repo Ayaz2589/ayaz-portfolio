ce65c10ec81a7a01065663d8f80afaff6cc4f4c2
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-09-05 19:24:23 -0400

    update

--- src/Components/CaseStudies.tsx
+++ src/Components/CaseStudies.tsx
@@ -9,6 +9,7 @@ import {
 import Link from "next/link";
 
 import PropertyPalLogoSVG from "./PropertyPalLogoSVG";
+import JetBlueLogoSVG from "./JetBlueLogoSVG";
 
 const style = {
   height: "100vh",
@@ -26,7 +27,7 @@ const cardStyles = {
 };
 
 const textStyle = {
-  width: { base: "90%", md: "25rem" },
+  width: { base: "90%", md: "30rem" },
   margin: { base: "0 1rem", md: "0rem 0rem 0rem 4rem" },
   fontWeight: "light",
   fontSize: { base: "1rem", md: "1.15rem" },
@@ -49,7 +50,7 @@ const CaseStudies = ({ scrollPosition, caseStudiesRef }: IProps) => {
       >
         <Box
           sx={{
-            width: { base: "", md: "40%" },
+            width: { base: "", md: "50%" },
             position: "relative",
             opacity: 0,
           }}
@@ -63,7 +64,7 @@ const CaseStudies = ({ scrollPosition, caseStudiesRef }: IProps) => {
         </Box>
         <Box
           sx={{
-            width: { base: "", md: "60%" },
+            width: { base: "", md: "50%" },
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
@@ -71,7 +72,7 @@ const CaseStudies = ({ scrollPosition, caseStudiesRef }: IProps) => {
           }}
         >
           <PropertyPalCaseStudy scrollPosition={scrollPosition} />
-          <PropertyPalCaseStudy scrollPosition={scrollPosition} />
+          <JetBlueCaseStudy scrollPosition={scrollPosition} />
         </Box>
       </Box>
     </Box>
@@ -93,11 +94,16 @@ const CaseStudyDetails = () => {
           Case Studies
         </Text>
         <Text sx={textStyle}>
-          With seven years of experience as a front-end developer, I've had the
-          privilege of working closely with design teams throughout my career,
-          which has honed my eye for user-centric design principles. Recently, I
-          made the exciting transition from front-end development to product
-          design, bolstered by my UX/UI certification from BrainStation
+          In my product design portfolio, I've embarked on two impactful
+          projects. The first project, "Property Pal," is an app designed to
+          simplify multi-home property management, specifically catering to
+          elderly property owners. It prioritizes user-friendliness and
+          affordability, significantly improving their property management
+          experience. In the second project, I led a comprehensive redesign of
+          JetBlue's trip booking feature, focusing on enhancing
+          user-friendliness and efficiency. This overhaul exemplifies my
+          commitment to user-centric design, ultimately elevating the usability
+          and satisfaction of travelers when planning their journeys.
         </Text>
       </Box>
     </Box>
@@ -107,7 +113,7 @@ const CaseStudyDetails = () => {
 const PropertyPalCaseStudy = ({ scrollPosition }: any) => {
   return (
     <Card
-      sx={{...cardStyles, opacity: 0}}
+      sx={{ ...cardStyles, opacity: 0 }}
       className={
         scrollPosition > 2100
           ? `paragraph-animation-down-appear`
@@ -121,14 +127,13 @@ const PropertyPalCaseStudy = ({ scrollPosition }: any) => {
         <CardBody sx={{ padding: "0rem 1rem 0rem 1rem" }}>
           <Text
             sx={{
-              textAlign: "center",
               fontSize: { base: "1.2rem", md: "1.1rem" },
               color: "white",
               fontWeight: "bold",
             }}
           >
-            A Property Management tool aimed at owners of multi-family home(s),
-            with an emphises placed on ease of use and afforibility.
+            A property management tool targeting multi-family home owners, with
+            a strong emphasis on user-friendliness and affordability.
           </Text>
         </CardBody>
         <CardFooter>
@@ -141,6 +146,50 @@ const PropertyPalCaseStudy = ({ scrollPosition }: any) => {
             }}
           >
             Click to View
+          </Text>
+        </CardFooter>
+      </Link>
+    </Card>
+  );
+};
+
+const JetBlueCaseStudy = ({ scrollPosition }: any) => {
+  return (
+    <Card
+      sx={{ ...cardStyles, opacity: 0, backgroundColor: "#d1d1d1" }}
+      className={
+        scrollPosition > 2100
+          ? `paragraph-animation-down-appear`
+          : `paragraph-animation-up-disappear`
+      }
+    >
+      <Link href="https://www.figma.com/proto/YHKLRhhrPw9K2L2Rs2M92Y/Property-Pal---Presentation?page-id=0%3A1&type=design&node-id=76-1150&viewport=10136%2C7619%2C1.15&t=5L2OkM3D2eXDdgfV-1&scaling=min-zoom&starting-point-node-id=76%3A1150&mode=design">
+        <CardHeader>
+          <JetBlueLogoSVG />
+        </CardHeader>
+        <CardBody sx={{ padding: "0rem 1rem 0rem 1rem" }}>
+          <Text
+            sx={{
+              fontSize: { base: "1.2rem", md: "1.1rem" },
+              color: "white",
+              fontWeight: "bold",
+            }}
+          >
+            An update of JetBlue's trip booking feature, prioritizing
+            user-friendliness and accessibility to provide travelers with a
+            smoother and more inclusive booking experience.
+          </Text>
+        </CardBody>
+        <CardFooter>
+          <Text
+            sx={{
+              fontSize: "0.8rem",
+              color: "white",
+              width: "100%",
+              textAlign: "right",
+            }}
+          >
+            Will be availsable soon
           </Text>
         </CardFooter>
       </Link>
