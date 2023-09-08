96c1124ce2d2c6376f16fb6168152bb2cf6116a1
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-09-08 16:03:13 -0400

    update

--- src/Components/svg/index.ts
+++ src/Components/svg/index.ts
@@ -1,10 +1,7 @@
 export { default as CollaborationSVG } from "./CollaborationSVG";
-export { default as FigmaSVG } from "./FigmaSVG";
 export { default as GmailLogoSVG } from "./GmailLogoSVG";
-export { default as JavascriptSVG } from "./JavascriptSVG";
 export { default as JetBlueLogoSVG } from "./JetBlueLogoSVG";
 export { default as LinkedInLogoSVG } from "./LinkedInLogoSVG";
 export { default as PropertyPalLogoSVG } from "./PropertyPalLogoSVG";
-export { default as ResearchSVG } from "./ResearchSVG";
 export { default as GithubLogoSVG } from "./GithubLogoSVG";
 export { default as LaptopSVG } from "./LaptopSVG";
